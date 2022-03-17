f98c4ac3d3903f2fb7e42cbbcf4f76cfe031e5bc
antonikarwowski <229908@edu.p.lodz.pl>
2022-03-17 20:38:24 +0100

    jp2gmd

--- cyfrowe_przetwarzanie_sygnalow/zadanie_1/Signal.py
+++ cyfrowe_przetwarzanie_sygnalow/zadanie_1/Signal.py
@@ -1,32 +1,42 @@
 import json
 import matplotlib.pyplot as plt
 
+from Parameters import Parameters
+
 
 def generate_ticks(function, parameters):
     try:
-        return [function(parameters, x/parameters.f) for x in range((parameters.d - parameters.t1) * parameters.f)]
+        return [function(parameters, x / parameters.f) for x in range((parameters.d - parameters.t1) * parameters.f)]
     except Exception:
         print("empty parameter in function " + function.__name__)
         return [0]
 
 
 class Signal:
-    def __init__(self, function, parameters):
+    def __init__(self, parameters, ticks, type):
         self.parameters = parameters
-        self.type = function.__name__
-        self.ticks = generate_ticks(function, parameters)
+        self.ticks = ticks
+        self.type = type
+
+    @staticmethod
+    def generate(function, parameters):
+        return Signal(parameters, generate_ticks(function, parameters), function.__name__)
 
     def print_plot(self, linear=True):
         plt.figure().suptitle(self.type)
         if linear:
-            plt.plot([i/self.parameters.f for i in range(len(self.ticks))], self.ticks)
+            plt.plot([i / self.parameters.f for i in range(len(self.ticks))], self.ticks)
         else:
-            plt.scatter([i/self.parameters.f for i in range(len(self.ticks))], self.ticks, s=1)
+            plt.scatter([i / self.parameters.f for i in range(len(self.ticks))], self.ticks, s=1)
         plt.show()
 
     def print_histogram(self, divisions_number):
         plt.hist(self.ticks, bins=divisions_number)
         plt.show()
+
+    def print_stats(self):
+        print("------stats-------")
+        print("chuj: " + str(len(self.ticks)))
 
     def serialize(self, filename):
         with open(filename, "w") as file:
@@ -34,7 +44,21 @@ class Signal:
 
     @staticmethod
     def deserialize(filename):
-        return json.loads(filename, object_hook=lambda d: Signal(**d))
-
-
-
+        with open(filename, "r") as file:
+            dictionary = json.loads(file.read())
+            signal = Signal(
+                Parameters(
+                    A=dictionary['parameters']['A'],
+                    T=dictionary['parameters']['T'],
+                    t1=dictionary['parameters']['t1'],
+                    d=dictionary['parameters']['d'],
+                    kw=dictionary['parameters']['kw'],
+                    ts=dictionary['parameters']['ts'],
+                    f=dictionary['parameters']['f'],
+                    p=dictionary['parameters']['p'],
+                    n1=dictionary['parameters']['n1'],
+                    ns=dictionary['parameters']['ns']
+                ),
+                dictionary['ticks'],
+                dictionary['type'])
+            return signal
---
+++ cyfrowe_przetwarzanie_sygnalow/zadanie_1/cli.py
@@ -0,0 +1,78 @@
+def execute():
+    print("->", end='')
+    commands = input().split(" ")
+
+    if commands[0] == "ls":
+        ls()
+    elif commands[0] == "help":
+        help()
+    elif commands[0] == "exit":
+        print_colored("exiting... good bye :)", WARNING)
+        exit(0)
+    elif commands[0] == "generate":
+        print(commands[0])
+    elif commands[0] == "sum":
+        print(commands[0])
+    elif commands[0] == "subtract":
+        print(commands[0])
+    elif commands[0] == "multiply":
+        print(commands[0])
+    elif commands[0] == "divide":
+        print(commands[0])
+    elif commands[0] == "plot":
+        print(commands[0])
+    elif commands[0] == "hist":
+        print(commands[0])
+    elif commands[0] == "stat":
+        print(commands[0])
+    else:
+        print_colored("wrong command, use help", FAIL)
+
+
+def ls():
+    print_colored("there are no saved signals", WARNING)
+
+
+def generate():
+    print_colored("generate", WARNING)
+
+
+def generate():
+    print_colored("generate", WARNING)
+
+
+def help():
+    print("------------------------------------------------------------------------------")
+    print_colored("management: ", WARNING)
+    print("    ls" + HEADER + " - list all saved impulses" + ENDC)
+    print("    help" + HEADER + " - shows this help" + ENDC)
+    print("    exit" + HEADER + " - exit program" + ENDC)
+    print_colored("generating: ", WARNING)
+    print("    generate" + OKCYAN + " <function name> <function arguments> <file name>" + ENDC)
+    print_colored("calculating: ", WARNING)
+    print("    sum" + OKCYAN + " <first input name> <second input name> <output name>" + ENDC)
+    print("    subtract" + OKCYAN + " <first input name> <second input name> <output name>" + ENDC)
+    print("    multiply" + OKCYAN + " <first input name> <second input name> <output name>" + ENDC)
+    print("    divide" + OKCYAN + " <first input name> <second input name> <output name>" + ENDC)
+    print_colored("statistic: ", WARNING)
+    print("    plot" + OKCYAN + " <file name>" + ENDC)
+    print("    hist" + OKCYAN + " <file name> <number of divisions>" + ENDC)
+    print("    stat" + OKCYAN + " <file name>" + ENDC)
+    print("------------------------------------------------------------------------------")
+    print(ENDC)
+
+
+def print_colored(text, color):
+    print(color + text + ENDC)
+
+
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKCYAN = '\033[96m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+
