0dfe60efd447435707a3fa26cb47fb6f194d07ba
antonikarwowski <229908@edu.p.lodz.pl>
2022-03-19 10:24:48 +0100

    asdf

--- techniki_sztucznej_inteligencji/zadanie_1/swarm/swarm.py
+++ techniki_sztucznej_inteligencji/zadanie_1/swarm/swarm.py
@@ -22,12 +22,13 @@ def particle_swarm_optimization_algorithm(
     for _ in range(iteration_number):
         for particle in swarm:
             particle.find_best_adaptation()
-            particle.calculate_new_velocity()
+            particle.calculate_new_velocity(global_best_position)
             particle.move_to_new_position()
 
             if particle.best_adaptation < global_best_adaptation:
                 best_positions.append(particle.best_position)
                 global_best_adaptation = particle.best_adaptation
+        global_best_position = best_positions[-1]
         best_adaptations.append(global_best_adaptation)
 
     return best_adaptations[-1]
