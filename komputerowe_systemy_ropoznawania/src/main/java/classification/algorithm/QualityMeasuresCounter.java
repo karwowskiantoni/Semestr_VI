7eea20ffd5a9c132b3cc3a1ee4e9e0b4ac4b8870
MichalGebel-TomTom <michal.gebel@tomtom.com>
2022-03-26 16:58:53 +0100

    add quality measures

---
+++ komputerowe_systemy_ropoznawania/src/main/java/classification/algorithm/QualityMeasuresCounter.java
@@ -0,0 +1,84 @@
+package classification.algorithm;
+
+import classification.model.Label;
+import classification.model.Text;
+
+import java.util.List;
+
+public class QualityMeasuresCounter {
+    public static float accuracy(List<Text> textList) {
+        int TP = 0;
+        for (Text text : textList) {
+            if (text.correctLabel() == text.predictedLabel()) {
+                TP += 1;
+            }
+        }
+        return TP * 1.0f / textList.size();
+    }
+
+    public static float precisionForSet(List<Text> textList) {
+        Label[] labels = Label.values();
+        float PPV = 0.0f;
+        for (Label label : labels) {
+            long occurrences = textList
+                    .stream()
+                    .filter(text -> text.correctLabel() == label)
+                    .count();
+            float precision = precisionPerClass(textList, label);
+            PPV += (occurrences * precision) / occurrences;
+        }
+        return PPV;
+    }
+
+    public static float recallForSet(List<Text> textList) {
+        Label[] labels = Label.values();
+        float TPR = 0.0f;
+        for (Label label : labels) {
+            long occurrences = textList
+                    .stream()
+                    .filter(text -> text.correctLabel() == label)
+                    .count();
+            float recall = recallPerClass(textList, label);
+            TPR += (occurrences * recall) / occurrences;
+        }
+        return TPR;
+    }
+
+    public static float F1(List<Text> textList) {
+        float PPV = precisionForSet(textList);
+        float TPR = recallForSet(textList);
+        return 2 * ((TPR * PPV) / (TPR + PPV));
+    }
+
+    public static float precisionPerClass(List<Text> textList, Label label) {
+        int TP = 0;
+        int FP = 0;
+        for (Text text : textList) {
+            if (label == text.correctLabel() && text.correctLabel() == text.predictedLabel()) {
+                TP += 1;
+            } else if (label != text.correctLabel() && text.correctLabel() != text.predictedLabel()) {
+                FP += 1;
+            }
+        }
+        return TP * 1.0f / (TP + FP);
+    }
+
+    public static float recallPerClass(List<Text> textList, Label label) {
+        int TP = 0;
+        int FN = 0;
+        for (Text text : textList) {
+            if (text.correctLabel() == label && text.correctLabel() == text.predictedLabel()) {
+                TP += 1;
+            } else if (label == text.correctLabel()) {
+                FN += 1;
+            }
+        }
+        return TP * 1.0f / (TP + FN);
+    }
+
+    public static float F1PerClass(List<Text> textList, Label label) {
+        float PPV = precisionPerClass(textList, label);
+        float TPR = recallPerClass(textList, label);
+        return 2 * ((TPR * PPV) / (TPR + PPV));
+    }
+}
