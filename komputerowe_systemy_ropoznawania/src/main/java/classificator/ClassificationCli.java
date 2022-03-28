6a44742334b8df1484a1465722d351446b4dbfbe
antonikarwowski <229908@edu.p.lodz.pl>
2022-03-28 08:38:01 +0200

    help

--- komputerowe_systemy_ropoznawania/src/main/java/classificator/ClassificationCli.java
+++ komputerowe_systemy_ropoznawania/src/main/java/classificator/ClassificationCli.java
@@ -7,7 +7,10 @@ import classificator.model.internal.Result;
 import classificator.model.internal.Text;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Callable;
 
@@ -18,16 +21,42 @@ import java.util.concurrent.Callable;
     description = "returns quality measures of classification algorithm")
 public class ClassificationCli implements Callable<Integer> {
 
-  //  @Parameters(index = "0", description = "The file whose checksum to calculate.")
-  //  private File file;
+  @Option(
+      names = {"-k", "--K"},
+      description = "K, number of nearest neighbours considered in classification algorithm",
+      showDefaultValue = CommandLine.Help.Visibility.ALWAYS)
+  private Integer K = 10;
 
-  //  @Option(names = {"-a", "--algorithm"}, description = "MD5, SHA-1, SHA-256, ...")
-  //  private String algorithm = "SHA-256";
+  @Option(
+      names = {"-ds", "--datasize"},
+      description = "size of texts set from database used for classification (chosen randomly)",
+      showDefaultValue = CommandLine.Help.Visibility.ALWAYS)
+  private Integer dsSize = 1000;
+
+  @Option(
+      names = {"-ts", "-testsize"},
+      description = "size of texts training set (chosen randomly, shouldn't be bigger than number parameter)",
+      showDefaultValue = CommandLine.Help.Visibility.ALWAYS)
+  private Integer tsSize = 200;
+
+  @Option(
+      names = {"-m", "--metric"},
+      description = "distance metric used for classification, (chebyshev/euclidean/taxicab)",
+      showDefaultValue = CommandLine.Help.Visibility.ALWAYS)
+  private String metricType = "chebyshev";
+
+  public static List<Text> randomChoices(List<Text> textList, int size) {
+    textList = new ArrayList<>(textList);
+    Collections.shuffle(textList);
+    return textList.subList(0, size);
+  }
 
   @Override
   public Integer call() throws Exception {
     List<Text> texts = Extractor.extractAll();
-    List<Result> results = Classificator.classifyAll(texts, texts, 100);
+    List<Text> textsToClassify = randomChoices(texts, dsSize);
+    List<Text> trainingTexts = randomChoices(textsToClassify, tsSize);
+    List<Result> results = Classificator.classifyAll(textsToClassify, trainingTexts, K);
     QualityMeasuresCalculator.printStatistics(results);
     return 0;
   }
--- komputerowe_systemy_ropoznawania/src/main/java/classificator/classification/Classificator.java
+++ komputerowe_systemy_ropoznawania/src/main/java/classificator/classification/Classificator.java
@@ -9,27 +9,22 @@ import java.util.*;
 public class Classificator {
 
   public static List<Result> classifyAll(List<Text> texts, List<Text> trainingTexts, int K) {
-    return texts.stream().map(Classificator::classifyOne).toList();
+    return texts.stream().map(text -> classifyOne(text, trainingTexts, K)).toList();
   }
 
-  public static Result classifyOne(Text text) {
+  public static Result classifyOne(Text text, List<Text> trainingTexts, int K) {
     return new Result(Label.CANADA, Label.CANADA);
-//    return trainingList.stream()
-//        .collect(
-//            Collectors.groupingBy(
-//                Text::label,
-//                Collectors.summingDouble(trainingText -> distance(text, trainingText))))
-//        .entrySet()
-//        .stream()
-//        .max(Comparator.comparingDouble(Map.Entry::getValue))
-//        .map(Map.Entry::getKey).orElse(null);
+    //    return trainingList.stream()
+    //        .collect(
+    //            Collectors.groupingBy(
+    //                Text::label,
+    //                Collectors.summingDouble(trainingText -> distance(text, trainingText))))
+    //        .entrySet()
+    //        .stream()
+    //        .max(Comparator.comparingDouble(Map.Entry::getValue))
+    //        .map(Map.Entry::getKey).orElse(null);
   }
 
-//  public static List<Text> generateTrainingSet(List<Text> textList, int size) {
-//    textList = new ArrayList<>(textList);
-//    Collections.shuffle(textList);
-//    return textList.subList(0, size);
-//  }
 
   private float distance(Text firstText, Text secondText) {
     return DistanceMetricsCalculator.euclideanDistance(firstText.vector(), secondText.vector());
