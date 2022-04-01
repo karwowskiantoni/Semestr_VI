41c29bc25f0744a354a75eb893d947972879b2a5
MichalGebel-TomTom <michal.gebel@tomtom.com>
2022-04-01 19:37:39 +0200

    add distance metrics

--- komputerowe_systemy_ropoznawania/src/main/java/classificator/classification/DistanceMetricsCalculator.java
+++ komputerowe_systemy_ropoznawania/src/main/java/classificator/classification/DistanceMetricsCalculator.java
@@ -2,16 +2,51 @@ package classificator.classification;
 
 import classificator.model.internal.Vector;
 
+import java.util.List;
+
 public class DistanceMetricsCalculator {
-    public static float taxiCabDistance (Vector firstVector, Vector secondVector){
-        return 1.0f * firstVector.firstDate() + secondVector.firstDate();
+    public static int euclideanDistance(Vector firstVector, Vector secondVector) {
+        return (int) (Math.pow(SimilarityMeasuresCalculator.wordSimilarity(firstVector.firstWordInDateline(), secondVector.firstWordInDateline()), 2)
+                + Math.pow(SimilarityMeasuresCalculator.wordSimilarity(firstVector.firstCountryInTitle(), secondVector.firstCountryInTitle()), 2)
+                + Math.pow(SimilarityMeasuresCalculator.wordSimilarity(firstVector.mostCommonWord(), secondVector.mostCommonWord()), 2)
+                + Math.pow(firstVector.firstDate() - secondVector.firstDate(), 2)
+                + Math.pow(firstVector.lastDate() - secondVector.lastDate(), 2)
+                + Math.pow(firstVector.westGermanyWordCount() - secondVector.westGermanyWordCount(), 2)
+                + Math.pow(firstVector.usaWordCount() - secondVector.usaWordCount(), 2)
+                + Math.pow(firstVector.franceWordCount() - secondVector.franceWordCount(), 2)
+                + Math.pow(firstVector.ukWordCount() - secondVector.ukWordCount(), 2)
+                + Math.pow(firstVector.canadaWordCount() - secondVector.canadaWordCount(), 2)
+                + Math.pow(firstVector.japanWordCount() - secondVector.japanWordCount(), 2));
     }
 
-    public static float euclideanDistance (Vector firstVector, Vector secondVector){
-        return 1.0f * firstVector.firstDate() + secondVector.firstDate();
+    public static int taxiCabDistance(Vector firstVector, Vector secondVector) {
+        return SimilarityMeasuresCalculator.wordSimilarity(firstVector.firstWordInDateline(), secondVector.firstWordInDateline())
+                + SimilarityMeasuresCalculator.wordSimilarity(firstVector.firstCountryInTitle(), secondVector.firstCountryInTitle()) +
+                SimilarityMeasuresCalculator.wordSimilarity(firstVector.mostCommonWord(), secondVector.mostCommonWord()) +
+                Math.abs(firstVector.firstDate() - secondVector.firstDate()) +
+                Math.abs(firstVector.lastDate() - secondVector.lastDate()) +
+                Math.abs(firstVector.westGermanyWordCount() - secondVector.westGermanyWordCount()) +
+                Math.abs(firstVector.usaWordCount() - secondVector.usaWordCount()) +
+                Math.abs(firstVector.franceWordCount() - secondVector.franceWordCount()) +
+                Math.abs(firstVector.ukWordCount() - secondVector.ukWordCount()) +
+                Math.abs(firstVector.canadaWordCount() - secondVector.canadaWordCount()) +
+                Math.abs(firstVector.japanWordCount() - secondVector.japanWordCount());
     }
 
-    public static float chebyshevDistance (Vector firstVector, Vector secondVector){
-        return 1.0f * firstVector.firstDate() + secondVector.firstDate();
+    public static int chebyshevDistance(Vector firstVector, Vector secondVector) {
+        List<Integer> distances = List.of(
+                SimilarityMeasuresCalculator.wordSimilarity(firstVector.firstWordInDateline(), secondVector.firstWordInDateline()),
+                SimilarityMeasuresCalculator.wordSimilarity(firstVector.firstCountryInTitle(), secondVector.firstCountryInTitle()),
+                SimilarityMeasuresCalculator.wordSimilarity(firstVector.mostCommonWord(), secondVector.mostCommonWord()),
+                Math.abs(firstVector.firstDate() - secondVector.firstDate()),
+                Math.abs(firstVector.lastDate() - secondVector.lastDate()),
+                Math.abs(firstVector.westGermanyWordCount() - secondVector.westGermanyWordCount()),
+                Math.abs(firstVector.usaWordCount() - secondVector.usaWordCount()),
+                Math.abs(firstVector.franceWordCount() - secondVector.franceWordCount()),
+                Math.abs(firstVector.ukWordCount() - secondVector.ukWordCount()),
+                Math.abs(firstVector.canadaWordCount() - secondVector.canadaWordCount()),
+                Math.abs(firstVector.japanWordCount() - secondVector.japanWordCount())
+        );
+        return distances.stream().mapToInt(Integer::intValue).max().getAsInt();
     }
 }
--- komputerowe_systemy_ropoznawania/src/main/java/classificator/classification/SimilarityMeasuresCalculator.java
+++ komputerowe_systemy_ropoznawania/src/main/java/classificator/classification/SimilarityMeasuresCalculator.java
@@ -2,7 +2,6 @@ package classificator.classification;
 
 import org.apache.commons.text.similarity.LevenshteinDistance;
 public class SimilarityMeasuresCalculator {
-
     public static int wordSimilarity(String firstWord, String secondWord) {
         return LevenshteinDistance.getDefaultInstance().apply(firstWord, secondWord);
     }
