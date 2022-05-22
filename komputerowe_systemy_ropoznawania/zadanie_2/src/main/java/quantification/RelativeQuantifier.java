c43230aa437626bdf1b7eadcfdddcf7cf936c3ae
MichalGebel-TomTom <michal.gebel@tomtom.com>
2022-05-22 22:16:41 +0200

    Added placeholder summarizer

--- komputerowe_systemy_ropoznawania/zadanie_2/src/main/java/quantification/RelativeQuantifier.java
+++ komputerowe_systemy_ropoznawania/zadanie_2/src/main/java/quantification/RelativeQuantifier.java
@@ -1,4 +1,16 @@
 package quantification;
 
+import static membership.MembershipLabel.*;
+
 public class RelativeQuantifier {
+    public static double calculateTypeMembership(RelativeQuantifierMembershipLabel label, double sigmaCount, double cardinality){
+        //values for relative membership is not yet done, so it's placeholder
+        return switch (label){
+            case VERY_SMALL_AMOUNT -> 1.0;
+            case SMALL_AMOUNT -> 2.0;
+            case MEDIUM_AMOUNT -> 3.0;
+            case HIGH_AMOUNT -> 4.0;
+            case VERY_HIGH_AMOUNT -> 5.0;
+        };
+    }
 }
---
+++ komputerowe_systemy_ropoznawania/zadanie_2/src/main/java/summarization/Summarizer.java
@@ -0,0 +1,20 @@
+package summarization;
+
+public class Summarizer {
+
+    public static double simpleSummarizationFirstType() {
+        return 0;
+    }
+
+    public static double complexSummarizationFirstType() {
+        return 0;
+    }
+
+    public static double simpleSummarizationSecondType() {
+        return 0;
+    }
+
+    public static double complexSummarizationSecondType() {
+        return 0;
+    }
+}
