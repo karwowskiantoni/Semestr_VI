849325615743448a24c62b0ec68a156c55bf9607
MichalGebel-TomTom <michal.gebel@tomtom.com>
2022-06-04 19:19:12 +0200

    ha tfu

--- komputerowe_systemy_ropoznawania/zadanie_2/src/main/java/linguisticsummary/database/Initialization.java
+++ komputerowe_systemy_ropoznawania/zadanie_2/src/main/java/linguisticsummary/database/Initialization.java
@@ -1,82 +1,92 @@
 package linguisticsummary.database;
 
-import javafx.util.Pair;
 import linguisticsummary.model.Meal;
 import linguisticsummary.model.MealLabel;
 import linguisticsummary.model.Quantifier;
-import linguisticsummary.model.Label;
 
 import java.util.List;
 
+import static linguisticsummary.model.Label.FunctionType.GAUSS;
 import static linguisticsummary.model.Label.FunctionType.TRAPEZIUM;
 
 public class Initialization {
     public static void initialize() {
+        List<Double> depressivenessDomain = List.of(0.0, 250.0);
+        List<Double> digestionDomain = List.of(0.0, 1.0);
+        List<Double> fatnessDomain = List.of(0.0, 100.0);
+        List<Double> healthinessDomain = List.of(0.0, 1.0);
+        List<Double> proteinDomain = List.of(0.0, 80.0);
+        List<Double> calorieDomain = List.of(0.0, 902.0);
+        List<Double> sweetDomain = List.of(0.0, 20.0);
+        List<Double> waterDomain = List.of(0.0, 100.0);
+
         List<MealLabel> mealLabels = List.of(
-                new MealLabel("LITTLE DIGESTED", TRAPEZIUM, List.of(80.0, 80.0, 83.0, 85.0), List.of(0.0, 100.0), Meal::absorption),
-                new Label("MEDIOCRE DIGESTED", meal -> trapezium(meal.absorption(), 80, 85, 94, 97)),
-                new Label("FULLY DIGESTED", meal -> trapezium(meal.absorption(), 95, 97, 99.8, 99.8)),
+                new MealLabel("LITTLE DIGESTED", TRAPEZIUM, List.of(80.0, 80.0, 83.0, 85.0), List.of(80.0, 85.0), Meal::absorption),
+                new MealLabel("MEDIOCRE DIGESTED", TRAPEZIUM, List.of(80.0, 85.0, 94.0, 97.0), List.of(80.0, 96.0), Meal::absorption),
+                new MealLabel("FULLY DIGESTED", TRAPEZIUM, List.of(95.0, 97.0, 99.8, 99.8), List.of(95.0, 100.0), Meal::absorption),
 
-                new Label("AVOIDABLE FOR DIABETICS", meal -> gauss(meal.adaptationForDiabetics(), 0, 0.1)),
-                new Label("NEUTRAL FOR DIABETICS", meal -> gauss(meal.adaptationForDiabetics(), 0.4, 0.1)),
-                new Label("DESTINED FOR DIABETICS", meal -> gauss(meal.adaptationForDiabetics(), 1, 0.2)),
+                new MealLabel("AVOIDABLE FOR DIABETICS", GAUSS, List.of(0.0, 0.1), List.of(0.0, 0.35), Meal::adaptationForDiabetics),
+                new MealLabel("NEUTRAL FOR DIABETICS", GAUSS, List.of(0.4, 0.1), List.of(0.1, 0.67), Meal::adaptationForDiabetics),
+                new MealLabel("DESTINED FOR DIABETICS", GAUSS, List.of(1.0, 0.2), List.of(0.3, 1.0), Meal::adaptationForDiabetics),
 
-                new Label("NO IMPACT ON DEPRESSION", meal -> trapezium(meal.antidepressiveness(), 0, 0, 30, 50)),
-                new Label("LITTLE IMPACT ON DEPRESSION", meal -> trapezium(meal.antidepressiveness(), 40, 60, 80, 100)),
-                new Label("IMPACTFUL ON DEPRESSION", meal -> trapezium(meal.antidepressiveness(), 80, 100, 110, 120)),
-                new Label("HIGHLY ANTIDEPRESSANT", meal -> trapezium(meal.antidepressiveness(), 100, 120, 250, 250)),
+                new MealLabel("NO IMPACT ON DEPRESSION", TRAPEZIUM, List.of(0.0, 0.0, 30.0, 50.0), List.of(0.0, 50.0), Meal::antidepressiveness),
+                new MealLabel("LITTLE IMPACT ON DEPRESSION", TRAPEZIUM, List.of(40.0, 60.0, 80.0, 100.0), List.of(0.0, 50.0), Meal::antidepressiveness),
+                new MealLabel("IMPACTFUL ON DEPRESSION", TRAPEZIUM, List.of( 80.0, 100.0, 110.0, 120.0), List.of(0.0, 50.0), Meal::antidepressiveness),
+                new MealLabel("HIGHLY ANTIDEPRESSANT", TRAPEZIUM, List.of( 100.0, 120.0, 250.0, 250.0), List.of(0.0, 50.0), Meal::antidepressiveness),
 
-                new Label("SLUGGISH DIGESTION", meal -> gauss(meal.digestionSpeed(), 0, 0.1)),
-                new Label("SLOW DIGESTION", meal -> gauss(meal.digestionSpeed(), 0.3, 0.1)),
-                new Label("STANDARD DIGESTION", meal -> gauss(meal.digestionSpeed(), 0.5, 0.1)),
-                new Label("FAST DIGESTION", meal -> gauss(meal.digestionSpeed(), 0.7, 0.1)),
-                new Label("RAPID DIGESTION", meal -> gauss(meal.digestionSpeed(), 1, 0.1)),
 
-                new Label("ALMOST WITHOUT FAT", meal -> trapezium(meal.fatness(), 0, 0, 5, 10)),
-                new Label("LITTLE FAT", meal -> trapezium(meal.fatness(), 6, 12, 18, 20)),
-                new Label("FAT", meal -> trapezium(meal.fatness(), 15, 25, 30, 35)),
-                new Label("HIGH FAT", meal -> trapezium(meal.fatness(), 30, 40, 50, 60)),
-                new Label("EXTREMELY FAT", meal -> trapezium(meal.fatness(), 40, 60, 100, 100)),
+                new MealLabel("SLUGGISH DIGESTION", GAUSS, List.of(0.0, 0.1), digestionDomain, Meal::digestionSpeed),
+                new MealLabel("SLOW DIGESTION", GAUSS, List.of(0.3, 0.1), digestionDomain, Meal::digestionSpeed),
+                new MealLabel("STANDARD DIGESTION", GAUSS, List.of(0.5, 0.1), digestionDomain, Meal::digestionSpeed),
+                new MealLabel("FAST DIGESTION", GAUSS, List.of(0.7, 0.1), digestionDomain, Meal::digestionSpeed),
+                new MealLabel("RAPID DIGESTION", GAUSS, List.of(1.0, 0.1), digestionDomain, Meal::digestionSpeed),
 
-                new Label("UNHEALTHY", meal -> gauss(meal.healthiness(), 0, 0.2)),
-                new Label("HEALTHY", meal -> gauss(meal.healthiness(), 0.6, 0.13)),
-                new Label("HEALTHFUL", meal -> gauss(meal.healthiness(), 1, 0.1)),
+                new MealLabel("ALMOST WITHOUT FAT", TRAPEZIUM, List.of(0.0, 0.0, 5.0, 10.0), fatnessDomain, Meal::fatness),
+                new MealLabel("LITTLE FAT", TRAPEZIUM, List.of(6.0, 12.0, 18.0, 20.0), fatnessDomain, Meal::fatness),
+                new MealLabel("FAT", TRAPEZIUM, List.of(15.0, 25.0, 30.0, 35.0), fatnessDomain, Meal::fatness),
+                new MealLabel("HIGH FAT", TRAPEZIUM, List.of(30.0, 40.0, 50.0, 60.0), fatnessDomain, Meal::fatness),
+                new MealLabel("EXTREMELY FAT", TRAPEZIUM, List.of(40.0, 60.0, 100.0, 100.0), fatnessDomain, Meal::fatness),
 
-                new Label("LITTLE PROTEIN", meal -> trapezium(meal.proteinContent(), 0, 0, 5, 10)),
-                new Label("MEDIOCRE PROTEIN", meal -> trapezium(meal.proteinContent(), 7, 12, 20, 30)),
-                new Label("HIGH PROTEIN", meal -> trapezium(meal.proteinContent(), 20, 30, 88.32, 88.32)),
+                new MealLabel("UNHEALTHY", GAUSS, List.of(0.0, 0.2), healthinessDomain, Meal::healthiness),
+                new MealLabel("HEALTHY", GAUSS, List.of(0.6, 0.13), healthinessDomain, Meal::healthiness),
+                new MealLabel("HEALTHFUL", GAUSS, List.of(1.0, 0.1), healthinessDomain, Meal::healthiness),
 
-                new Label("VERY LOW CALORIE", meal -> trapezium(meal.satiety(), 0, 0, 70, 130)),
-                new Label("LOW CALORIE", meal -> trapezium(meal.satiety(), 100, 170, 200, 250)),
-                new Label("MEDIUM CALORIE", meal -> trapezium(meal.satiety(), 200, 300, 400, 600)),
-                new Label("HIGH CALORIE", meal -> trapezium(meal.satiety(), 500, 550, 700, 800)),
-                new Label("EXTREMELY HIGH CALORIE", meal -> trapezium(meal.satiety(), 700, 850, 902, 902)),
+                new MealLabel("LITTLE PROTEIN", TRAPEZIUM, List.of(0.0, 0.0, 5.0, 10.0), proteinDomain, Meal::proteinContent),
+                new MealLabel("MEDIOCRE PROTEIN", TRAPEZIUM, List.of(7.0, 12.0, 20.0, 30.0), proteinDomain, Meal::proteinContent),
+                new MealLabel("HIGH PROTEIN", TRAPEZIUM, List.of(20.0, 30.0, 88.32, 88.32), proteinDomain, Meal::proteinContent),
 
-                new Label("NO SWEET", meal -> trapezium(meal.sweetness(), 0, 0, 1, 2)),
-                new Label("SWEET", meal -> trapezium(meal.sweetness(), 0.5, 2, 6, 8)),
-                new Label("VERY SWEET", meal -> trapezium(meal.sweetness(), 5, 9, 13, 15)),
-                new Label("MADE OF SUGAR", meal -> trapezium(meal.sweetness(), 13, 16, 20, 20)),
+                new MealLabel("VERY LOW CALORIE", TRAPEZIUM, List.of(0.0, 0.0, 70.0, 130.0), calorieDomain, Meal::satiety),
+                new MealLabel("LOW CALORIE", TRAPEZIUM, List.of(100.0, 170.0, 200.0, 250.0), calorieDomain, Meal::satiety),
+                new MealLabel("MEDIUM CALORIE", TRAPEZIUM, List.of(200.0, 300.0, 400.0, 600.0), calorieDomain, Meal::satiety),
+                new MealLabel("HIGH CALORIE", TRAPEZIUM, List.of(500.0, 550.0, 700.0, 800.0), calorieDomain, Meal::satiety),
+                new MealLabel("EXTREMELY HIGH CALORIE", TRAPEZIUM, List.of(700.0, 850.0, 902.0, 902.0), calorieDomain, Meal::satiety),
 
-                new Label("ALMOST WITHOUT WATER", meal -> trapezium(meal.wateriness(), 0, 0, 7, 20)),
-                new Label("HYDRATING", meal -> trapezium(meal.wateriness(), 15, 25, 40, 50)),
-                new Label("WATER RICH", meal -> trapezium(meal.wateriness(), 30, 60, 80, 90)),
-                new Label("ENTIRELY WATER", meal -> trapezium(meal.wateriness(), 80, 90, 100, 100)));
+                new MealLabel("NO SWEET", TRAPEZIUM, List.of(0.0, 0.0, 1.0, 2.0), sweetDomain, Meal::sweetness),
+                new MealLabel("SWEET", TRAPEZIUM, List.of(0.5, 2.0, 6.0, 8.0), sweetDomain, Meal::sweetness),
+                new MealLabel("VERY SWEET", TRAPEZIUM, List.of(5.0, 9.0, 13.0, 15.0), sweetDomain, Meal::sweetness),
+                new MealLabel("MADE OF SUGAR", TRAPEZIUM, List.of(13.0, 16.0, 20.0, 20.0), sweetDomain, Meal::sweetness),
+
+
+                new MealLabel("ALMOST WITHOUT WATER", TRAPEZIUM, List.of(0.0, 0.0, 7.0, 20.0), waterDomain, Meal::wateriness),
+                new MealLabel("HYDRATING", TRAPEZIUM, List.of(15.0, 25.0, 40.0, 50.0), waterDomain, Meal::wateriness),
+                new MealLabel("WATER RICH", TRAPEZIUM, List.of(30.0, 60.0, 80.0, 90.0), waterDomain, Meal::wateriness),
+                new MealLabel("ENTIRELY WATER", TRAPEZIUM, List.of(80.0, 90.0, 100.0, 100.0), waterDomain, Meal::wateriness)
+        );
 
         List<Quantifier> quantifiers = List.of(
                 new Quantifier( "LESS THAN 2000", TRAPEZIUM, List.of(0.0, 0.0, 1700.0, 1800.0), List.of(0.0, 1800.0), true),
+                new Quantifier( "ABOUT 2500", TRAPEZIUM, List.of(1500.0, 1800.0, 3400.0, 3600.0), List.of(1500.0, 3600.0), true),
+                new Quantifier( "ABOUT 5000", TRAPEZIUM, List.of(3400.0, 3600.0, 5200.0, 5400.0), List.of(3400.0, 5400.0), true),
+                new Quantifier( "ABOUT 6500", TRAPEZIUM, List.of(5200.0, 5400.0, 7000.0, 7200.0), List.of(5200.0, 7200.0), true),
+                new Quantifier( "MORE THAN 7000", TRAPEZIUM, List.of(7000.0, 7200.0, 8792.0, 8792.0), List.of(7000.0, 8792.0), true),
 
-                new Quantifier(new Pair(1500.0, 3600.0), List.of(1500.0, 1800.0, 3400.0, 3600.0), "ABOUT 2500", membershipDegree -> trapezium(membershipDegree, 1500, 1800, 3400, 3600), true, false),
-                new Quantifier(new Pair(3400.0, 5400.0), List.of(3400.0, 3600.0, 5200.0, 5400.0), "ABOUT 5000", membershipDegree -> trapezium(membershipDegree, 3400, 3600, 5200, 5400), true, false),
-                new Quantifier(new Pair(5200.0, 7200.0), List.of(5200.0, 5400.0, 7000.0, 7200.0), "ABOUT 6500", membershipDegree -> trapezium(membershipDegree, 5200, 5400, 7000, 7200), true, false),
-                new Quantifier(new Pair(7000.0, 8792.0), List.of(7000.0, 7200.0, 8792.0, 8792.0), "MORE THAN 7000", membershipDegree -> trapezium(membershipDegree, 7000, 7200, 8792, 8792), true, false),
-
-                new Quantifier(new Pair(0.0, 0.35), List.of(0.0, 0.1), "VERY SMALL AMOUNT", membershipDegree -> gauss(membershipDegree, 0, 0.1), false, true),
-                new Quantifier(new Pair(0.0, 0.55), List.of(0.25, 0.1), "SMALL AMOUNT", membershipDegree -> gauss(membershipDegree, 0.25, 0.1), false, true),
-                new Quantifier(new Pair(0.25, 0.75), List.of(0.5, 0.7), "MEDIUM AMOUNT", membershipDegree -> gauss(membershipDegree, 0.5, 0.7), false, true),
-                new Quantifier(new Pair(0.4, 1.0), List.of(0.75, 0.1), "HIGH AMOUNT", membershipDegree -> gauss(membershipDegree, 0.75, 0.1), false, true),
-                new Quantifier(new Pair(0.65, 1.0), List.of(1.0, 0.1), "VERY HIGH AMOUNT", membershipDegree -> gauss(membershipDegree, 1, 0.1), false, true)
-        );
-        VariableDatabase.saveAll(mealLabels);
+                new Quantifier( "VERY SMALL AMOUNT", GAUSS, List.of(0.0, 0.1), List.of(0.0, 0.35), false),
+                new Quantifier( "SMALL AMOUNT", GAUSS, List.of(0.25, 0.1), List.of(0.0, 0.55), false),
+                new Quantifier( "MEDIUM AMOUNT", GAUSS, List.of(0.5, 0.7), List.of(0.25, 0.75), false),
+                new Quantifier( "HIGH AMOUNT", GAUSS, List.of(0.75, 0.1), List.of(0.4, 0.35), false),
+                new Quantifier( "VERY HIGH AMOUNT", GAUSS, List.of(1.0, 0.1), List.of(0.65, 1.0), false)
+                );
+        MealLabelsDatabase.saveAll(mealLabels);
         QuantifierDatabase.saveAll(quantifiers);
     }
 
