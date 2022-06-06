e16313cdf077c7d8a6db8d4aecd9deba4966f25b
MichalGebel-TomTom <michal.gebel@tomtom.com>
2022-06-06 07:29:27 +0200

    gowno

--- komputerowe_systemy_ropoznawania/zadanie_2/src/main/java/linguisticsummary/view/Controller.java
+++ komputerowe_systemy_ropoznawania/zadanie_2/src/main/java/linguisticsummary/view/Controller.java
@@ -1,53 +1,23 @@
 package linguisticsummary.view;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
-import linguisticsummary.model.Qualifier;
-import linguisticsummary.model.Summarizer;
-import linguisticsummary.summary.SingleEntitySummarySecondForm;
-import linguisticsummary.database.Initialization;
-import linguisticsummary.database.MealDatabase;
-import linguisticsummary.database.MealLabelDatabase;
-import linguisticsummary.database.QuantifierDatabase;
-import linguisticsummary.model.Meal;
-import linguisticsummary.model.MealLabel;
-import linguisticsummary.model.Quantifier;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static linguisticsummary.LinguisticSummary.allCombinations;
 
 public class Controller {
     @FXML
-    public Button randomSummariesButton;
+    private TableView<String> table;
+
     @FXML
     private TextArea summariesList;
 
-//    @FXML
-//    protected void generateSummaries() {
-//        Initialization.initialize();
-//
-//        List<Meal> meals = MealDatabase.loadAll();
-//        List<Quantifier> allQuantifiers = QuantifierDatabase.loadAll();
-//        List<MealLabel> allLabels = MealLabelDatabase.loadAll();
-//
-//        List<SingleEntitySummarySecondForm> allSummaries = new ArrayList<>();
-//
-//        for(List<MealLabel> labels : allCombinations(allLabels.stream().limit(10).toList(), 2)) {
-//            for(Quantifier quantifier: allQuantifiers) {
-//                allSummaries.add(new SingleEntitySummarySecondForm(quantifier, new Qualifier(List.of(labels.get(0))), new Summarizer(List.of(labels.get(1))), meals));
-//            }
-//        }
-//        allSummaries.sort(Comparator.comparingDouble(value -> value.measures().getOptimalSummary()));
-//        Collections.reverse(allSummaries);
+    @FXML
+    private void initialize(){
+        String o = table.getColumns().get(0);
+    }
 
-//        var best = allSummaries.stream().filter(singleEntitySummarySecondForm -> !Double.isNaN(singleEntitySummarySecondForm.measures().getOptimalSummary())).limit(20).toList();
-//        String text = best.stream().map(SingleEntitySummarySecondForm::toString).collect(Collectors.joining("\n"));
-//        summariesList.setText(text);
-//    }
+    @FXML
+    protected void generateSummaries() {
+
+    }
 }
